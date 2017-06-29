9b2cedb17dcbc4755293e3aba2edbe3c0db0ca31
Jaxon <jexcoon@gmail.com>
2017-06-29 11:51:00 -0500

    cleanish

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Armor or Weapon?");
         Scanner scannerStep1 = new Scanner(System.in);
         String scannerText = scannerStep1.nextLine();
+        System.out.println("");
 
         if(scannerText.equalsIgnoreCase("Armor")) {
             armorCall();
@@ -29,21 +30,28 @@ public class Main {
         Scanner scannerStep2 = new Scanner(System.in);
         System.out.println("Please enter the current min damage.");
         minDamage = scannerStep2.nextInt();
+        System.out.println("");
 
         Scanner scannerStep3 = new Scanner(System.in);
         System.out.println("Now enter the current max damage.");
         maxDamage = scannerStep3.nextInt();
+        System.out.println("");
+
 
         Scanner scannerStep4 = new Scanner(System.in);
         System.out.println("Now enter the plus you would like it to be");
         plus = scannerStep4.nextInt();
+        System.out.println("");
 
         for(int i = 0; i < plus; i++){
             maxDamage = maxDamage + maxDamage * 5/100;
             minDamage = minDamage + minDamage * 5/100;
         }
-        System.out.println("Your weapon +" + plus + "ed will be");
+        System.out.println("Your weapon +" + plus + "ed will be\n");
         System.out.println("DMG: " + minDamage + " - " + maxDamage);
+
+        System.out.println("\n\n\nPress enter to exit");
+        String end = new Scanner(System.in).nextLine();
     }
 
     public static void armorCall() {
@@ -57,16 +65,18 @@ public class Main {
         Scanner scannerStep2 = new Scanner(System.in);
         System.out.println("Please enter the current health on the armor piece.");
         health = scannerStep2.nextInt();
+        System.out.println("");
 
 
         Scanner scannerStep3 = new Scanner(System.in);
-        System.out.println("Is the piece Energy or HP REGEN?\n");
+        System.out.println("Is the piece Energy or HP REGEN?");
         String scannerIn1 = scannerStep3.nextLine();
         if(scannerIn1.equalsIgnoreCase("HP REGEN")) {
             hpsBoolean = true;
         }else if(scannerIn1.equalsIgnoreCase("Energy")) {
             energyBoolean = true;
         }
+        System.out.println("");
 
         Scanner scannerStep4 = new Scanner(System.in);
         if(energyBoolean == true) {
@@ -77,10 +87,12 @@ public class Main {
             System.out.println("What is the current HP REGEN on the armor piece?");
             hps = scannerStep4.nextInt();
         }
+        System.out.println("");
 
         Scanner scannerStep5 = new Scanner(System.in);
         System.out.println("Now enter the plus you would like it to be");
         plus = scannerStep5.nextInt();
+        System.out.println("");
 
         for(int i = 0; i < plus; i++){
             health = health + health * 5/100;
@@ -91,7 +103,7 @@ public class Main {
                 hps = hps + hps * 5/100;
             }
         }
-        System.out.println("Your Armor +" + plus + "ed will be");
+        System.out.println("Your Armor +" + plus + "ed will be\n");
         System.out.println("HP: +" + health);
         if(energyBoolean) {
             System.out.println("ENERGY REGEN: +" + energy + "%");
@@ -99,5 +111,7 @@ public class Main {
         if(hpsBoolean) {
             System.out.println("HP REGEN: +" + hps + " HP/s");
         }
+        System.out.println("\n\n\nPress enter to exit");
+        String end = new Scanner(System.in).nextLine();
     }
 }
